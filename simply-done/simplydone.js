5e92724b6be5d78c90f0ae6be15b9c229f851fad
Anderson-Packer <apacker7.23@gmail.com>
2025-12-12 20:43:33 -0700

    go go power rangers

--- simply-done/simplydone.js
+++ simply-done/simplydone.js
@@ -1,36 +1,75 @@
 console.log("JS is connected!");
- 
-const add = document.querySelector(".add")
+
+const add = document.querySelector(".add");
 const list = document.querySelector(".list");
-const remove = document.querySelector(".remove")
-const clear = document.querySelector(".clear")
+const remove = document.querySelector(".remove");
+const clear = document.querySelector(".clear");
+
+let items = [];
 
 function addItem() {
-        const newItem = document.createElement("div");
-    newItem.classList.add("item");
+    const form = document.createElement("form");
+    form.classList.add("item");
 
-    newItem.innerHTML = `
-        <form class="item" onsubmit="return false">
-            <label for="item"><img src="circle.jpg" class="circle" alt="empty circle"></label>
-            <input type="text" class="input" name="item">
-            <label for="date" class="center">DATE:</label>
-            <input type="date" class="date" name="date">
-       </form>
+    form.innerHTML = `
+        <label>
+            <img src="circle.jpg" class="circle" alt="empty circle">
+        </label>
+        <input type="text" class="input" placeholder="New task" required>
+        <label class="center">DATE:</label>
+        <input type="date" class="date">
     `;
-    list.appendChild(newItem);
-    console.log("item added");
+
+    form.addEventListener("submit", submitItem);
+    list.appendChild(form);
 }
 
-add.addEventListener("click", addItem)
+function submitItem(e) {
+    e.preventDefault();
 
- remove.addEventListener("click", () => {
-    const lastItem = list.lastElementChild;
-    
-    lastItem.remove();
-    console.log("item removed")
-});
+    const form = e.target;
+    const text = form.querySelector(".input").value;
+    const date = form.querySelector(".date").value;
 
-clear.addEventListener("click", () => {
+    if (!text) return;
+
+    const itemObj = {
+        text: text,
+        date: date
+    };
+
+    items.push(itemObj);
+    renderItems();
+}
+
+function renderItems() {
     list.innerHTML = "";
-});
 
+    items.forEach(item => {
+        const div = document.createElement("div");
+        div.classList.add("item");
+
+        div.innerHTML = `
+            <img src="circle.jpg" class="circle" alt="empty circle">
+            <span class="input">${item.text}</span>
+            <span class="center date">${item.date || ""}</span>
+        `;
+
+        list.appendChild(div);
+    });
+}
+
+function removeItem() {
+    if (items.length === 0) return;
+    items.pop();
+    renderItems();
+}
+
+function clearItems() {
+    items = [];
+    renderItems();
+}
+
+add.addEventListener("click", addItem);
+remove.addEventListener("click", removeItem);
+clear.addEventListener("click", clearItems);
